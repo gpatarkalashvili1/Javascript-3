617dd0d57463b75e8a0a9e725fa8309c758835da
giopatarkalashvili <114107995+gpatarkalashvili1@users.noreply.github.com>
2022-12-12 15:26:22 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,118 @@
+// 1
+
+// let array = [5, 25, 89, 120, 36];
+// array.push ('javascript','python')
+// array.unshift('html','css');
+// console.log(array.length);
+// array.shift();
+// array.pop();
+// console.log(array);
+
+
+// 2
+
+// let array = ['orange','banana','pear'];
+// console.log(array.length);
+// array.push('apple','pineapple');
+// array.unshift('watermelon');
+// console.log(array.length);
+// array.splice(3,0,'mango')
+// array.shift
+// array.pop
+// console.log(array.length);
+// console.log(array);
+
+
+// 3
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let mapArray = array.map(function (item) {
+//     return item / 3;
+// })
+// console.log(mapArray);
+
+
+// 4 
+
+// let array = [
+//   "hello",
+//   125,
+//   "javascript",
+//   "html",
+//   43,
+//   "css",
+//   "scss",
+//   "bootstrap",
+//   88,
+//   59,
+//   "python",
+// ];
+
+// let filterArray = array.filter(function (item) {
+//   return typeof item === "number";
+// });
+
+// console.log(filterArray);
+
+
+// 5 
+
+
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+// let filterArray = languages.filter(function(item) {
+//     return  item.length > 3;
+// })
+
+// console.log(filterArray);
+
+
+// 6 
+
+// let array = [12, 'google', 32, null, 'yahoo', 25];
+
+// let mapArray = array.map(function (item) {
+//     if(typeof item === 'number'){
+//         return(item * item);
+//     }
+//     if(typeof item === 'string'){
+//         return item.toUpperCase();
+//     } else {
+//         return item;
+//     }
+// })
+// console.log(mapArray);
+
+
+// 7 
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+
+// let filterArray = words.filter(words => words.includes ('M') || words.includes ('m') )
+// console.log(filterArray);
+
+
+// 8 
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// arr.forEach(function (item) {
+//     return sum += item;
+// })
+// console.log(sum);
+
+
+// 9
+
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+
+// let numArray = numbers.filter(function (item) {
+//     return item > 0;
+// })
+// console.log(numArray);
+
+
+// 10
+
+// let array = [1, 2, 3, 4, 5];
+// array.splice(3,0,'a','b','c');
+// console.log(array);
